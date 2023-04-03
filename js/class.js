fb1512748aa37dad18ab70cdae23cbf539365880
lamhuynhb779 <lamhuynhb779@gmail.com>
2023-04-03 23:38:52 +0700

    first commit

---
+++ js/class.js
@@ -0,0 +1,14 @@
+const classes = [
+    'Knight',
+    'Warlock',
+    'Mage',
+    'Warrior',
+    'Hunter',
+    'Assasin',
+    'Shaman',
+    'Druid',
+    'Witcher',
+    'Mech',
+    'Priest',
+    'Wizard',
+];
